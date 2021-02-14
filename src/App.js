6c77af062ff541303bf230d4fbaeeb1d93393a78
Ivan Khomenko <Simkav2411@gmail.com>
2021-02-14 13:24:41 +0200

    Init project

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Calendar from './Components/Calendar/indes.jsx';
+require('./reset.css');
+require('./style.css');
+function App() {
+  return <Calendar />;
+}
+
+export default App;
---
+++ src/Components/Calendar/indes.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Calendar extends Component {
+  render() {
+    return (
+      <section className="calendar-container">
+        <div className="half current-day-view">
+          <div>
+            
+          </div>
+        </div>
+        <div className="half second">2</div>
+      </section>
+    );
+  }
+}
+
+export default Calendar;
